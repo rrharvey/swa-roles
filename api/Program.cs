e9212ec44df236fc40f83895f652723254b981d5
Ryan Harvey <happyharv@gmail.com>
2023-07-13 12:49:31 -0500

    try changing rolesSource

--- api/Program.cs
+++ api/Program.cs
@@ -14,21 +14,39 @@ builder.Logging.AddFilter<ApplicationInsightsLoggerProvider>("", LogLevel.Trace)
 
 var app = builder.Build();
 
-app.MapGet(
+app.MapPost(
     "/api/GetRoles",
     (ILogger<Program> logger) =>
     {
         logger.LogInformation("running /api/GetRoles");
-        return new[] { "foo", "bar" };
+        return new { roles = new[] { "foo", "bar" } };
     }
 );
 
-app.MapGet(
+app.MapPost(
     "/GetRoles",
     (ILogger<Program> logger) =>
     {
         logger.LogInformation("running /GetRoles");
-        return new[] { "lalala", "loopsie" };
+        return new { roles = new[] { "lala", "googoo" } };
+    }
+);
+
+app.MapPost(
+    "/roles",
+    (ILogger<Program> logger) =>
+    {
+        logger.LogInformation("running /roles");
+        return new { roles = new[] { "admin", "superpower" } };
+    }
+);
+
+app.MapPost(
+    "/api/roles",
+    (ILogger<Program> logger) =>
+    {
+        logger.LogInformation("running /api/roles");
+        return new { roles = new[] { "admin2", "superpower2" } };
     }
 );
 
